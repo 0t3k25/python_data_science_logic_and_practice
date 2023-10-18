247405143a4ad8257c77f0365b76121923fb4a4a
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-10-18 23:16:42 +0900

    encode class label by using lambda way

--- data_prep/one_hot_encoding.py
+++ data_prep/one_hot_encoding.py
@@ -23,3 +23,21 @@ c_transf = ColumnTransformer(
     [("one-hot", OneHotEncoder(), [0]), ("nothing", "passthrough", [1, 2])]
 )
 c_transf.fit_transform(X).astype(float)
+
+# クラスラベルのエンコーディング
+import numpy as np
+
+df = pd.DataFrame(
+    [
+        ["green", "M", "10.1", "class2"],
+        ["red", "L", "13.5", "class1"],
+        ["blue", "XL", "15.3", "class2"],
+    ]
+)
+df.columns = ["color", "size", "price", "classlabel"]
+# lambda式を使った方法
+# カラムの値が増える
+df["x > M"] = df["size"].apply(lambda x: 1 if x in {"L", "XL"} else 0)
+df["x > L"] = df["size"].apply(lambda x: 1 if x == "XL" else 0)
+del df["size"]
+df
