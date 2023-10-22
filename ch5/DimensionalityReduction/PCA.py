3c3b3d025bc4df3a5bbbd51ac0a27fd9843cc4ec
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-10-23 01:18:21 +0900

    add variance explained ratio

--- ch5/DimensionalityReduction/PCA.py
+++ ch5/DimensionalityReduction/PCA.py
@@ -27,3 +27,30 @@ import numpy as np
 cov_mat = np.cov(X_train_std.T)  # 共分散行列を作成
 eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)  # 固有値と固有ベクトルを計算
 print(f"Eigenvalue \n {eigen_vals}")
+# 分散説明率を求める
+# 固有値の合計
+tot = sum(eigen_vals)
+# print(tot)
+# 分散説明率を計算
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+# 分散説明率の累積和を取得
+# 累積和はその時点での分散の割合を示している
+cum_var_exp = np.cumsum(var_exp)
+print(var_exp)
+import matplotlib.pyplot as plt
+
+# 分散説明率の棒グラフを作成
+plt.bar(
+    range(1, 14),
+    var_exp,
+    alpha=0.5,
+    align="center",
+    label="Individual explained variance",
+)
+# 分散説明率の累積和の段階グラフを作成
+plt.step(range(1, 14), cum_var_exp, where="mid", label="Cumulative explained variance")
+plt.ylabel("Explained variance ratio")
+plt.xlabel("principal component index")
+plt.legend(loc="best")
+plt.tight_layout()
+plt.show()
