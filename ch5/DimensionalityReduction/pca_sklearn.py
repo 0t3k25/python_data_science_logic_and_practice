896ca99d75f35892ca4cebb37cf8c697022df989
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-10-24 22:05:26 +0900

    implement pca using sklearn

---
+++ ch5/DimensionalityReduction/pca_sklearn.py
@@ -0,0 +1,20 @@
+# sklearnを用いてpcaを実装
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+
+# 主成分類を指定して、PCAのインスタンスを生成
+pca = PCA(n_components=2)
+# ロジスティック回帰のインスタンスを生成
+lr = LogisticRegression(multi_class="ovr", random_state=1, solver="lbfgs")
+# 訓練データとテストデータを次元削減
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+# 削減したデータセットでロジスティック回帰モデルを適合
+lr.fit(X_train_pca, y_train)
+# 決定境界をプロット
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.legend(loc="lower left")
+plt.tight_layout()
+plt.show()
