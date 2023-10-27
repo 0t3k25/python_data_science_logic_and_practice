2cb3b1fed73383099865f1c9c04e7a27deeb44eb
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-10-27 23:50:12 +0900

    visualize discriminability ratio by pyplot

--- ch5/LDA/calculate_eigen_vals.py
+++ ch5/LDA/calculate_eigen_vals.py
@@ -7,3 +7,23 @@ eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
 print("Eigenvalues in descending order:\n")
 for eigen_val in eigen_pairs:
     print(eigen_val[0])
+# 線形判別をプロット
+# 固有値の実巣部の総和を求める
+tot = sum(eigen_vals.real)
+# 分散説明率とその累積和を計算
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discur = np.cumsum(discr)
+plt.bar(
+    range(1, 14),
+    discr,
+    alpha=0.5,
+    align="center",
+    label='Individual "discriminability"',
+)
+plt.step(range(1, 14), cum_discr, where="mid", label='cumulative "discriminability"')
+plt.ylabel('"Discriminability" ratio')
+plt.xlabel("Linear Discriminants")
+plt.ylim([-0.1, 1.1])
+plt.legend(loc="best")
+plt.tight_layout()
+plt.show()
