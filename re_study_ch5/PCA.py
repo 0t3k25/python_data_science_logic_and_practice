df36af393790037076d3007479e68a9cb14707db
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-10-29 23:40:23 +0900

    restart study about pca and lda

---
+++ re_study_ch5/PCA.py
@@ -0,0 +1,8 @@
+# principal component analysis
+# read data set
+import pandas as pd
+
+df_wine = pd.read_csv(
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",
+    header=None,
+)
