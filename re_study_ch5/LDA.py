05c9dfb6e7d4f833fe304c6124efb1c55a765a2a
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-11-06 23:10:25 +0900

    wip:implement LDA
    calculate within-class scatter matrix

--- re_study_ch5/LDA.py
+++ re_study_ch5/LDA.py
@@ -6,4 +6,25 @@ for label in range(1, 4):
     mean_vecs.append(np.mean(X_train_std[y_train == label], axis=0))
     print(f"MV {label}: {mean_vecs[label-1]}\n")
 d = 13
+# クラス内変動行列の計算
 S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.zeros((d, d))
+    for row in X_train_std[y_train == label]:
+        row, mv = row.reshape(d, 1), mv.reshape(d, 1)
+        # 変動行列Siを合計
+        class_scatter += (row - mv).dot((row - mv).T)
+    S_W += class_scatter
+# クラス内変動行列の次元
+print(f"Within-class scatter matrix: {S_W.shape[0]}x{S_W.shape[1]}")
+# クラスのラベル存在数
+print(f"Class label distribution: {np.bincount(y_train)[1:]}")
+
+# クラスラベルが一様に分布していないためスケーリング実行
+# クラス内変動行列
+d = 13
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.cov(X_train_std[y_train == label].T)
+    S_W += class_scatter
+print(f"Scaled within-class scatter matrix {S_W.shape[0]}x{S_W.shape[1]}")
