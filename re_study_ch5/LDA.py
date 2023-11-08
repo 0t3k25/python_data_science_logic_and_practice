26eafb31d5d32221058fb955d97d0cf0443caf2e
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-11-08 22:08:39 +0900

    Add decision region visualization for wine dataset
    - Utilized LogisticRegression model with LDA preprocessing on wine dataset.

--- re_study_ch5/LDA.py
+++ re_study_ch5/LDA.py
@@ -36,7 +36,7 @@ S_B = np.zeros((d, d))
 for i, mean_vec in enumerate(mean_vecs):
     # 各クラスに属するサンプル数を取得
     n = X_train_std[y_train == i + 1, :].shape[0]
-    mena_vec = mean_vec.reshape(d, 1)  # 列ベクトルを作成
+    mean_vec = mean_vec.reshape(d, 1)  # 列ベクトルを作成
     mean_overall = mean_overall.reshape(d, 1)
     S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
 print(f"Between-class scatter matrix: {S_B.shape[0]}x{S_B.shape[1]}")
@@ -62,5 +62,52 @@ plt.ylabel('"Discriminability" ratio')
 plt.xlabel("Linear Discriminants")
 plt.ylim([-0.1, 1.1])
 plt.legend(loc="best")
+plt.tight_layout()
+plt.show()
+# Discriminabilityの高い二つの固有ベクトル
+# を列方向に並べて変換行列Wを作成
+w = np.hstack(
+    (eigen_pairs[0][1][:, np.newaxis].real, eigen_pairs[1][1][:, np.newaxis].real)
+)
+print(f"Matrix W:\n{w}")
+# 標準化した訓練データに変換行列を掛ける
+X_train_lda = X_train_std.dot(w)
+colors = ["r", "b", "g"]
+markers = ["s", "x", "o"]
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(
+        X_train_lda[y_train == l, 0],
+        X_train_lda[y_train == l, 1] * (-1),
+        c=c,
+        label=l,
+        marker=m,
+    )
+plt.xlabel("LD1")
+plt.ylabel("LD2")
+plt.legend(loc="lower right")
+plt.tight_layout()
+plt.show()
+# sklearnを利用した線形判別分析
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+# 次元数を指定して、LDAのインスタンスを生成
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+# ロジスティック回帰
+# 訓練データの分類
+lr = LogisticRegression(multi_class="ovr", random_state=1, solver="lbfgs")
+lr = lr.fit(X_train_lda, y_train)
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel("LD1")
+plt.ylabel("LD2")
+plt.legend(loc="lower left")
+plt.tight_layout()
+plt.show()
+# テストデータでの分類
+X_test_lda = lda.transform(X_test_std)
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.xlabel("LD1")
+plt.ylabel("LD2")
+plt.legend(loc="lower left")
 plt.tight_layout()
 plt.show()
