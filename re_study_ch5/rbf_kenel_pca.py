a732ea59009ef5dbf6c35e0637c184f7a7903ad5
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-11-09 22:52:44 +0900

    implement rbf kernel pca

---
+++ re_study_ch5/rbf_kenel_pca.py
@@ -0,0 +1,44 @@
+from scipy.spatial.distance import pdist, squareform
+from scipy import exp
+from scipy.linalg import eigh
+import numpy an np
+
+def rbf_kernel_pca(X,gamma, n_components):
+  """RBFカーネルPCAの実装
+
+  パラメータ
+  -----------
+  X: {Numpy ndarray}, shape = [n_examples, n_features]
+  gamma: float
+    RBFカーネルのチューニングパラメータ
+  n_components: int
+    返される主成分の個数
+  
+  戻り値
+  ---------
+  X_pc: {NumPy ndarray}, shape = [n_examples, k_features]
+    射影されたデータセット
+  """
+  
+  # M × N次元のデータセットでペアごとのユークリッド距離の2乗を計算
+  sp_dists = pdlist(X, 'sqeuclidean')
+
+  # ペアごとの距離を正方行列に変換
+  mat_sq_dists = squareform(sq_dists)
+
+  # 対象カーネル行列を計算
+  K = exp(-gamma * mat_sq_dists)
+
+  # カーネル行列を中心化
+  N = K.shape[0]
+  one_n = np.ones((N,N)) / N
+  K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+  # 中心化されたカーネル行列から固有対を取得:scipy.linalg.eighはそれらを昇順で返す
+  eigvals, eigvecs = eigh(K)
+  eigvals, eigvecs = eigvals[::-1], eigvecs[:,::-1]
+
+  # 上位k個の固有ベクトル(射影されたデータ点)を取得
+  X_pc = np.column_stack((eigvecs[:,i] for i in range(n_components)))
+  return X_pc
+  
