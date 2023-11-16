e3730cb5ee689e32cb839796b7acc23b4fd7aeac
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-11-16 23:18:00 +0900

    visualize accuracy vs Number of training examples

--- ch6/diagnose_learning_validation_curve.py
+++ ch6/diagnose_learning_validation_curve.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import learning_curve
 
 pipe_lr = make_pipeline(
     StandardScaler(),
-    LogisticRegression(penalty="12", random_tate=1, solver="lbfgs", max_iter=100),
+    LogisticRegression(penalty="12", random_state=1, solver="lbfgs", max_iter=100),
 )
 # learning_curve関数で交差検証による正解率を算出
 train_sizes, train_scores, test_scores = learning_curve(
@@ -24,7 +24,33 @@ plt.plot(
     color="blue",
     marker="o",
     markersize=5,
-    lavel="Training acccuracy",
+    label="Training acccuracy",
 )
 # fill_between関数で平均±標準偏差の幅を塗りつぶす
-plt.fill_between(train_sizes, train_mean + train_std)
+# 訓練データのサイズtrain_sizes,透明度alpha,カラー'blue'を引数に指定
+plt.fill_between(
+    train_sizes,
+    train_mean + train_std,
+    train_mean - train_std,
+    alpha=0.15,
+    color="blue",
+)
+plt.plot(
+    train_sizes,
+    test_mean,
+    color="green",
+    linestyle="--",
+    marker="s",
+    markersize=5,
+    label="Validation accuracy",
+)
+plt.fill_between(
+    train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color="green"
+)
+plt.grid()
+plt.xlabel("Number of training examples")
+plt.ylabel("Accuracy")
+plt.legend(loc="lower right")
+plt.ylim([0.8, 1.03])
+plt.tight_layout()
+plt.show()
