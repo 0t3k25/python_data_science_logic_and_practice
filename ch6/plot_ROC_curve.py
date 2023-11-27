8d0de3d4b20da2c0f832a8a4b1fe641663100067
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-11-27 23:54:07 +0900

    WIP:ROC AUC

--- ch6/plot_ROC_curve.py
+++ ch6/plot_ROC_curve.py
@@ -8,4 +8,28 @@ pipe_lr = make_pipeline(
     PCA(n_components=2),
     LogisticRegression(penalty="l2", random_state=1, slover="lbfgs", C=100.0),
 )
-#
+# 2つの特徴量を抽出
+X_train2 = X_train[:, [4, 14]]
+# 層化k分割交差検証イテレータを表すStratifiedKFoldクラスをインスタンス化
+cv = list(StratifiedKFold(n_splits=3).split(X_train, y_train))
+fig = plt.figure(figsize=(7, 5))
+mean_tpr = 0.0
+# 0から1までの間で100個の要素を生成
+mean_fpr = np.linespace(0, 1, 100)
+all_tpr = []
+for i, (train, test) in enumerate(cv):
+    # predict_probaメソッドで確率を予測、fitメソッドでモデルに適合させる
+    probas = pipe_lr.fit(
+        X_train2[train],
+        y_train[train],
+    ).predict_proba(X_train2[test])
+    # roc_curve関数でROC曲線の性能を計算してプロット
+    fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label=1)
+    mean_tpr += interp(mean_fpr, fpr, tpr)  # FPR(X軸)とTPR（Y軸）を線形補間
+    mean_tpr[0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, label=f"RoC fold {i+1} (area= {roc_auc:2f})")
+# 当て推量をプロット
+plt.plot([0, 1], [0, 1], linestyle="--", color=(0.6, 0.6, 0.6), label="Random guessing")
+# FPR, TPR, ROC AUCそれぞれの平均を計算してプロット
+mean_tpr /= len(cv)
