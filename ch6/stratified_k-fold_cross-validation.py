924a8d710c57e46bd1528b4a0c56069d296fd253
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-11-28 23:53:06 +0900

    implement k-fold cross validation

--- ch6/stratified_k-fold_cross-validation.py
+++ ch6/stratified_k-fold_cross-validation.py
@@ -1,33 +1,50 @@
-import numpy as np
-from sklearn.model_selection import StratifiedKFold
+# ROC曲線のプロット
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
 
-# 分割元データ、分割数、乱数生成機の状態を指定し、
-# 層化k分割交差検証イテレータを表すStratifiedKFoldクラスのインスタンス化
-kfold = StratifiedKFold(n_splits=10).split(X_train, y_train)
-scores = []
-# イテレータのインデックスと要素をループ処理：（上から順に）
-#   データをモデルに適合
-#   テストデータの正解率を算出
-#   リストに正解率を追加
-#   分割の番号、0　以上の要素数、正解率を出力
-
-for k, (train, test) in enumerate(kfold):
-    print(train)
-    print("train")
-    print(test)
-    pipe_lr.fit(X_train[train], y_train[train])
-    score = pipe_lr.score(X_train[test], y_train[test])
-    scores.append(score)
-    print(f"Fold: {k+1}, Class dist.: {np.bincount(y_train[train])}, Acc:{score:.3f}")
-# 正解率の平均と標準偏差を出力
-print(f"\nCV accuracy: {np.mean(scores):.3f} +/- {np.std(scores):.3f}")
-
-# 層化k分割交差検証を使ったモデルの評価
-# より簡単に評価を出すことができる
-from sklearn.model_selection import cross_val_score
-
-# 交差検証のcross_val_score缶sぬうでモデルの正解率を算出
-# 推定機esrimator、訓練データX、予測値y、分割数cv、CPU数n_jobsを指定
-scores = cross_val_score(estimator=pipe_lr, X=X_train, y=y_train, cv=10, n_jobs=1)
-print(f"CV accuracy scores:{scores}")
-print(f"CV accuracy:{np.mean(scores):.3f} +/- {np.std(scores):.3f}")
+# スケーリング、主成分分析、ロジスティック回帰を指定して、Pieplineクラスをインスタンス化
+pipe_lr = make_pipeline(
+    StandardScaler(),
+    PCA(n_components=2),
+    LogisticRegression(penalty="l2", random_state=1, solver="lbfgs", C=100.0),
+)
+# 2つの特徴量を抽出
+X_train2 = X_train[:, [4, 14]]
+# 層化k分割交差検証イテレータを表すStratifiedKFoldクラスをインスタンス化
+cv = list(StratifiedKFold(n_splits=3).split(X_train, y_train))
+fig = plt.figure(figsize=(7, 5))
+mean_tpr = 0.0
+# 0から1までの間で100個の要素を生成
+mean_fpr = np.linspace(0, 1, 100)
+all_tpr = []
+for i, (train, test) in enumerate(cv):
+    # predict_probaメソッドで確率を予測、fitメソッドでモデルに適合させる
+    probas = pipe_lr.fit(
+        X_train2[train],
+        y_train[train],
+    ).predict_proba(X_train2[test])
+    # roc_curve関数でROC曲線の性能を計算してプロット
+    fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label=1)
+    mean_tpr += interp(mean_fpr, fpr, tpr)  # FPR(X軸)とTPR（Y軸）を線形補間
+    mean_tpr[0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, label=f"RoC fold {i+1} (area= {roc_auc:2f})")
+# 当て推量をプロット
+plt.plot([0, 1], [0, 1], linestyle="--", color=(0.6, 0.6, 0.6), label="Random guessing")
+# FPR, TPR, ROC AUCそれぞれの平均を計算してプロット
+mean_tpr /= len(cv)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+plt.plot(mean_fpr, mean_tpr, "k--", label=f"Mean ROC (area = {mean_auc:2f})", lw=2)
+# 完全に予測が正解した時のROC曲線をプロット
+plt.plot(
+    [0, 0, 1], [0, 1, 1], linestyle=":", color="black", label="Perfect performance"
+)
+# グラフの各項目を指定ー
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel("False positive rate")
+plt.ylabel("True positive rate")
+plt.legend(loc="lower right")
+plt.tight_layout()
+plt.show()
