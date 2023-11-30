0ec672fe678ce8131d4470c4173c2c297426ea01
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-11-30 23:47:50 +0900

    Supplement shortage of for repetition

--- ch7/ensemble_error.py
+++ ch7/ensemble_error.py
@@ -5,7 +5,10 @@ import math
 
 def ensemble_error(n_classifier, error):
     k_start = int(math.ceil(n_classifier / 2.0))
-    probs = [comb(n_classifier, k) * error**k * (1 - error) ** (n_classifier + 1)]
+    probs = [
+        comb(n_classifier, k) * error**k * (1 - error) ** (n_classifier - k)
+        for k in range(k_start, n_classifier + 1)
+    ]
     return sum(probs)
 
 
