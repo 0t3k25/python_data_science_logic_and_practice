69daf8267e74c5567dc8e475c32dcf9867dd87fe
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-12-04 21:11:01 +0900

    visualize error  base classifier vs ensemble using pyplot

--- ch7/ensemble_error.py
+++ ch7/ensemble_error.py
@@ -13,3 +13,16 @@ def ensemble_error(n_classifier, error):
 
 
 ensemble_error(n_classifier=11, error=0.25)
+# ベース分類機とアンサンブル分類機の誤分類率
+import numpy as np
+import matplotlib.pyplot as plt
+
+error_range = np.arange(0.0, 1.01, 0.01)
+ens_errors = [ensemble_error(n_classifier=11, error=error) for error in error_range]
+plt.plot(error_range, ens_errors, label="Ensemble error", linewidth=2)
+plt.plot(error_range, error_range, linestyle="--", label="Base error", linewidth=2)
+plt.xlabel("Base error")
+plt.ylabel("Base/Ensemble error")
+plt.legend(loc="upper left")
+plt.grid(alpha=0.5)
+plt.show()
