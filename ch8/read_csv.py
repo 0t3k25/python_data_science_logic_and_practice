c03afdfbab6c0c52c3ef6c94653f3b63aa25e2c0
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-12-17 00:01:18 +0900

    confirm get data

--- ch8/read_csv.py
+++ ch8/read_csv.py
@@ -11,3 +11,13 @@ df = pd.read_csv(io.BytesIO(uploaded["movie_data.csv"]))
 
 df.columns = ["review", "sentiment"]
 print(df.columns)
+
+# ちゃんと情報が取れているか確認
+import numpy as np
+
+np.random.seed(0)
+df = df.reindex(np.random.permutation(df.index))
+df.to_csv("movie_data.csv", index=False, encoding="utf-8")
+df = pd.read_csv("movie_data.csv", encoding="utf-8")
+df.head(3)
+df.shape
