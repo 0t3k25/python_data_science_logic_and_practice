950c1f62a9347471e78d6ba5db3daed5a8cc1b6a
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-12-17 22:27:02 +0900

    evaluate word relationship
    -count each word appearance
    print tfidf

---
+++ ch8/count_vectorizer_and_print_tfidf.py
@@ -0,0 +1,21 @@
+# evaluate word importance
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+count = CountVectorizer()
+docs = np.array(
+    [
+        "The sun is shining",
+        "The weather is sweet",
+        "The sun is shining, the weather is sweet, and one and one is two",
+    ]
+)
+bag = count.fit_transform(docs)
+print(count.vocabulary_)
+print(bag.toarray())
+
+from sklearn.feature_extraction.text import TfidfTransformer
+
+tfidf = TfidfTransformer(use_idf=True, norm="l2", smooth_idf=True)
+np.set_printoptions(precision=2)
+print(tfidf.fit_transform(count.fit_transform(docs)).toarray())
