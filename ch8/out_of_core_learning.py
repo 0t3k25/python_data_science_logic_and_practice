4980703e86b3f501a67b4ab3a364bb80302db531
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2023-12-30 23:52:51 +0900

    wip: one by one out of core learning

--- ch8/out_of_core_learning.py
+++ ch8/out_of_core_learning.py
@@ -44,3 +44,15 @@ vect = HashingVectorizer(
 )
 clf = SGDClassifier(loss="log", random_state=1)
 doc_stream = stream_docs(path="/content/movie_data.csv")
+
+import pyprind
+
+pbar = pyprind.ProgBar(45)
+classes = np.array([0, 1])
+for _ in range(45):
+    X_train, y_train = get_minibatch(doc_stream, size=1000)
+    if not X_train:
+        break
+    X_train = vect.transform(X_train)
+    clf.partial_fit(X_train, y_train, classes=classes)
+    pbar.update()
