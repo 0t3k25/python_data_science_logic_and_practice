04701cca098a98201d6312174e4b3bfda08ce404
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-01-02 02:06:35 +0900

    confirm review

--- ch8/analysis_text_using_LDA.py
+++ ch8/analysis_text_using_LDA.py
@@ -6,4 +6,22 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 count = CountVectorizer(stop_words="english", max_df=0.1, max_features=5000)
 X = count.fit_transform(df["review"].values)
-print(X)
+
+# number of topics
+print(lda.components_.shape)
+
+n_top_words = 5
+feature_names = count.get_feature_names_out()
+
+# print topics
+for topic_idx, topic in enumerate(lda.components_):
+    print(f"Topic {topic_idx +1}")
+    print(
+        " ".join([feature_names[i] for i in topic.argsort()[: -n_top_words - 1 : -1]])
+    )
+
+horror = X_topics[:, 5].argsort()[::-1]
+
+for iter_idx, movie_idx in enumerate(horror[:3]):
+    print("\n Horror movie #%d" % (iter_idx + 1))
+    print(df["review"][movie_idx][:300], "...")
