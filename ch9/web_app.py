c5ed81da6356431fc0d6220acac015c46737a573
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-01-07 23:59:14 +0900

    deserialize

--- ch9/web_app.py
+++ ch9/web_app.py
@@ -7,3 +7,20 @@ if not os.path.exists(dest):
     os.makedirs(dest)
 pickle.dump(stop, open(os.path.join(dest, "stopwords.pkl"), "wb"), protocol=4)
 pickle.dump(clf, open(os.path.join(dest, "classifier.pkl"), "wb"), protocol=4)
+
+# deserialize
+import pickle
+import re
+import os
+from vectorizer import vect
+
+clf = pickle.load(open(os.path.join("pkl_objects", "classifier.pkl"), "rb"))
+
+import numpy as np
+
+label = {0: "negative", 1: "positive"}
+example = ["I love this movie. It's amazing."]
+X = vect.transform(example)
+print(
+    f"Prediction{label[clf.predict(X)[0]]} \nProbability: {np.max(clf.predict_proba(X))*100}"
+)
