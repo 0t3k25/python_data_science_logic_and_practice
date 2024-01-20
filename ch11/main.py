4dc57b7ddb1d58cf061b9908263f4d321c48bcab
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-01-20 22:18:02 +0900

    wip:confirm Clustering performance using silhouette analysis

--- ch11/main.py
+++ ch11/main.py
@@ -90,3 +90,31 @@ plt.xlabel("Number of clusters")
 plt.ylabel("Distortion")
 plt.tight_layout()
 plt.show()
+
+km = KMeans(
+    n_clusters=3, init="k-means++", n_init=10, max_iter=300, tol=1e-04, random_state=0
+)
+y_km = km.fit_predict(X)
+
+import numpy as np
+from matplotlib import cm
+from sklearn.metrics import silhouette_samples
+
+cluster_labels = np.unique(y_km)  # y_kmの要素の中で重複をなくす
+n_clusters = cluster_labels.shape[0]  # 配列の長さを返す
+# シルエット係数を計算
+silhouette_vals = silhouette_samples(X, y_km, metric="euclidean")
+y_ax_lower, y_ax_upper = 0, 0
+yticks = []
+for i, c in enumerate(cluster_labels):
+    c_silhouette_vals = silhouette_vals[y_km == c]
+    c_silhouette_vals.sort()
+    y_ax_upper += len(c_silhouette_vals)
+    color = cm.jet(float(i) / n_clusters)  # 色の値をセット
+    plt.barh(
+        range(y_ax_lower, y_ax_upper),
+        c_silhouette_vals,
+        height=1.0,
+        edgecolor="none",
+        color=color,
+    )
