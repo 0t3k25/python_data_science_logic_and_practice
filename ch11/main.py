c95566fa0fa7258744c80cd6606d07e76bc28d5a
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-01-23 23:56:45 +0900

    wip: implement hierarchical clustering

--- ch11/main.py
+++ ch11/main.py
@@ -133,3 +133,34 @@ plt.scatter(
     marker="s",
     label="Cluster 1",
 )
+
+# 階層的クラスタリング
+# データ作成
+import pandas as pd
+import numpy as np
+
+np.random.seed(123)
+variables = ["X", "Y", "Z"]
+labels = ["ID_0", "ID_1", "ID_2", "ID_3", "ID_4"]
+X = np.random.random_sample([5, 3]) * 10
+df = pd.DataFrame(X, columns=variables, index=labels)
+df
+# hierachicak clustering
+from scipy.spatial.distance import pdist, squareform
+
+# pdistで距離を計算、squareformで対称行列を生成
+row_dist = pd.DataFrame(
+    squareform(pdist(df, metric="euclidean")), columns=labels, index=labels
+)
+row_dist
+
+from scipy.cluster.hierarchy import linkage
+
+row_clusters = linkage(pdist(df, metric="euclidean"), method="complete")
+row_clusters
+
+pd.DataFrame(
+    row_clusters,
+    columns=["row label 1", "row label 2", "distance", "no. of items in clust."],
+    index=["cluster %d" % (i + 1) for i in range(row_clusters.shape[0])],
+)
