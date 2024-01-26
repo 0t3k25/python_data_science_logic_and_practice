bc972a95dc5bd4fc758fb5a32f03d9d6ce79fb3f
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-01-27 01:25:24 +0900

    visualize 7
    learn how to use compressed file

--- ch12/main.py
+++ ch12/main.py
@@ -34,11 +34,42 @@ X_test, y_test = load_mnist("/content/drive/My Drive", kind="t10k")
 print("Rows: %d, columns: %d" % (X_test.shape[0], X_test.shape[1]))
 
 # visualize image using matplotlib
-import matplotlib.puplot as plt
+import matplotlib.pyplot as plt
 
 # subplotsで描画を設定：引数で描画領域の行数/列数、x/y軸の統一を指定
 fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True)
 ax = ax.flatten()  # 配列を1次元に変形
 for i in range(10):
     img = X_train[y_train == i][0].reshape(28, 28)  # 配列を28×28に変形
-    ax[i].imshow(img, cmap="Gray")
+    ax[i].imshow(img, cmap="Greys")
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+# visualize other image
+fig, ax = plt.subplots(nrows=5, ncols=5, sharex=True, sharey=True)
+ax = ax.flatten()
+for i in range(25):
+    img = X_train[y_train == 7][i].reshape(28, 28)
+    ax[i].imshow(img, cmap="Greys")
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+# compressed
+import numpy as np
+
+np.savez_compressed(
+    "mnist_scaled.npz", X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
+)
+
+# fetch npz file
+mnist = np.load("mnist_scaled.npz")
+
+mnist.files
+
+X_train = mnist["X_train"]
+
+X_train, y_train, X_test, y_test = [mnist[f] for f in mnist.files]
