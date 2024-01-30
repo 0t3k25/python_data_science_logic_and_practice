dedaa96ae4e72147bc35446451e9562e24026464
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-01-30 22:52:01 +0900

    learn about tensor using tensorflow

---
+++ ch13/main.py
@@ -0,0 +1,13 @@
+# テンソルの確認
+import tensorflow as tf
+import numpy as np
+
+print("TensorFlow version:", tf.__version__)
+
+np.set_printoptions(precision=3)
+a = np.array([1, 2, 3], dtype=np.int32)
+b = [4, 5, 6]
+t_a = tf.convert_to_tensor(a)
+t_b = tf.convert_to_tensor(b)
+print(t_a)
+print(t_b)
