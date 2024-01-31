04cb0be7689c3b9506724a31dbb43a831935a885
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-01-31 22:22:37 +0900

    learn about tensorflow function

--- ch13/main.py
+++ ch13/main.py
@@ -11,3 +11,73 @@ t_a = tf.convert_to_tensor(a)
 t_b = tf.convert_to_tensor(b)
 print(t_a)
 print(t_b)
+
+t_ones = tf.ones((2, 3))
+t_ones.numpy()
+
+const_tensor = tf.constant([1.2, 5, np.pi], dtype=tf.float32)
+print(const_tensor)
+
+t_a_new = tf.cast(t_a, tf.int64)
+print(t_a_new.dtype)
+
+# テンソルの転置
+t = tf.random.uniform(shape=(3, 5))
+t_tr = tf.transpose(t)
+print(t.shape, "-->", t_tr.shape)
+
+# テンソルの形状変更
+t = tf.zeros((30,))
+print(t)
+t_reshape = tf.reshape(t, shape=(5, 6))
+print(t_reshape)
+
+# 不要な次元の削除
+t = tf.zeros((1, 2, 1, 4, 1))
+t_sqz = tf.squeeze(t, axis=(2, 4))  # 2,4を削除
+print(t.shape, "-->", t_sqz.shape)
+
+tf.random.set_seed(1)
+t1 = tf.random.uniform(shape=(5, 2), minval=-1.0, maxval=1.0)
+t2 = tf.random.normal(shape=(5, 2), mean=0.0, stddev=1.0)
+# print(t1,t2)
+
+t3 = tf.multiply(t1, t2).numpy()
+print(t3)
+
+t4 = tf.math.reduce_mean(t1, axis=0)
+print(t4)
+
+t5 = tf.linalg.matmul(t1, t2, transpose_b=True)
+print(t5)
+
+t6 = tf.linalg.matmul(t1, t2, transpose_a=True)
+print(t6)
+
+norm_t1 = tf.norm(t1, ord=2, axis=1).numpy()
+print(norm_t1)
+
+tf.random.set_seed(1)
+t = tf.random.uniform((6,))
+print(t.numpy())
+
+t_splits = tf.split(t, num_or_size_splits=3)
+[item.numpy() for item in t_splits]
+
+tf.random.set_seed(1)
+t = tf.random.uniform((5,))
+
+print(t.numpy())
+
+t_splits = tf.split(t, num_or_size_splits=[3, 2])
+[item.numpy() for item in t_splits]
+
+A = tf.ones((3,))
+B = tf.zeros((2,))
+C = tf.concat([A, B], axis=0)
+print(C.numpy())
+
+A = tf.ones((3,))
+B = tf.zeros((3,))
+S = tf.stack([A, B], axis=1)
+print(S.numpy())
