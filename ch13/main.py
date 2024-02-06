9f4f2d3154596dc9202882d98e9564c03c44a489
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-02-06 23:07:38 +0900

    load and create dataset

--- ch13/main.py
+++ ch13/main.py
@@ -127,3 +127,56 @@ tf.random.set_seed(1)
 ds = ds_joint.batch(2).shuffle(20).repeat(3)
 for i, (batch_x, batch_y) in enumerate(ds):
     print(i, batch_x.shape, batch_y.numpy())
+
+# 画像ファイル取得
+import pathlib
+
+imgdir_path = pathlib.Path("/content/cat_dog_images")
+file_list = sorted([str(path) for path in imgdir_path.glob("*.jpg")])
+
+print(file_list)
+
+import matplotlib.pyplot as plt
+import os
+
+fig = plt.figure(figsize=(10, 5))
+for i, file in enumerate(file_list):
+    img_raw = tf.io.read_file(file)
+    img = tf.image.decode_image(img_raw)
+    print("image shape: ", img.shape)
+    ax = fig.add_subplot(2, 3, i + 1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(img)
+    ax.set_title(os.path.basename(file), size=15)
+
+# labeling
+labels = [1 if "dog" in os.path.basename(file) else 0 for file in file_list]
+print(labels)
+
+# make dataset
+ds_file_labels = tf.data.Dataset.from_tensor_slices((file_list, labels))
+for item in ds_file_labels:
+    print(item[0].numpy(), item[1].numpy())
+
+
+# 前処理
+def load_and_preprocess(path, label):
+    image = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [img_height, img_width])
+    image /= 255.0
+    return image, label
+
+
+img_width, img_height = 120, 80
+ds_images_labels = ds_file_labels.map(load_and_preprocess)
+fig = plt.figure(figsize=(10, 5))
+for i, example in enumerate(ds_images_labels):
+    ax = fig.add_subplot(2, 3, i + 1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(example[0])
+    ax.set_title(f"{example[1].numpy()}", size=15)
+plt.tight_layout()
+plt.show()
