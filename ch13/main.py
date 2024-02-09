230c3e1899d01c7e22fd32eddb985a9e9cc46f4a
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-02-09 23:52:58 +0900

    linear regression using tensor

--- ch13/main.py
+++ ch13/main.py
@@ -211,3 +211,33 @@ for i, (image, label) in enumerate(zip(batch[0], batch[1])):
     ax.set_yticks([])
     ax.imshow(image[:, :, 0], cmap="gray_r")
     ax.set_title(f"{label}", size=15)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 線形回帰
+X_train = np.arange(10).reshape((10, 1))
+y_train = np.array([1.0, 1.3, 3.1, 2.0, 5.0, 6.3, 6.6, 7.4, 8.0, 9.0])
+plt.plot(X_train, y_train, "o", markersize=10)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+# 標準化
+import tensorflow as tf
+
+X_train_norm = X_train - np.mean(X_train) / np.std(X_train)
+ds_train_orig = tf.data.Dataset.from_tensor_slices(
+    (tf.cast(X_train_norm, tf.float32), tf.cast(y_train, tf.float32))
+)
+
+
+# keras
+class MyModel(tf.keras.Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.w = tf.Variable(0.0, name="weight")
+        self.b = tf.Bariable(0.0, name="bias")
+
+    def call(self, X):
+        return self.w * X + self.b
