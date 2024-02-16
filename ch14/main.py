f2c0c200d53b452508da795bbb405dc61958bd4d
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-02-16 23:44:38 +0900

    デコレーターを利用したtensorflowの使い方

--- ch14/main.py
+++ ch14/main.py
@@ -11,3 +11,34 @@ def compute_z(a, b, c):
     r2 = tf.multiply(2, r1)
     z = tf.add(r2, c)
     return z
+
+
+@tf.function
+def compute_z(a, b, c):
+    r1 = tf.subtract(a, b)
+    r2 = tf.multiply(2, r1)
+    z = tf.add(r2, c)
+    return z
+
+
+tf.print("Scalar Inputs:", compute_z(1, 2, 3))
+tf.print("Rank 1 Inputs:", compute_z([1], [2], [3]))
+tf.print("Rank 2 Inputs:", compute_z([[1]], [[2]], [[3]]))
+
+
+@tf.function(
+    input_signature=(
+        tf.TensorSpec(shape=[None], dtype=tf.int32),
+        tf.TensorSpec(shape=[None], dtype=tf.int32),
+        tf.TensorSpec(shape=[None], dtype=tf.int32),
+    )
+)
+def compute_z(a, b, c):
+    r1 = tf.subtract(a, b)
+    r2 = tf.multiply(2, r1)
+    z = tf.add(r2, c)
+    return z
+
+
+tf.print("Rank 1 Inputs:", compute_z([1], [2], [3]))
+tf.print("Rank 1 Inputs:", compute_z([1, 2], [2, 4], [3, 6]))
