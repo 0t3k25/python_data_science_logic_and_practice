9fe26c72e34c3ca1e8d3f115dd143ee4f0cd20d3
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-02-19 22:47:37 +0900

    calculate gradient using tf

--- ch14/main.py
+++ ch14/main.py
@@ -84,3 +84,27 @@ m = MyModule()
 print("All module variabels:", [v.shape for v in m.variables])
 
 print("Trainable variable:", [v.shape for v in m.trainable_variables])
+
+import tensorflow as tf
+
+w = tf.Variable(1.0)
+b = tf.Variable(0.5)
+print(w.trainable, b.trainable)
+
+x = tf.convert_to_tensor([1.4])
+y = tf.convert_to_tensor([2.1])
+
+with tf.GradientTape() as tape:
+    z = tf.add(tf.multiply(w, x), b)
+    loss = tf.reduce_sum(tf.square(y - z))
+
+dloss_dw = tape.gradient(loss, w)
+tf.print("dL/dw:", dloss_dw)
+
+with tf.GradientTape() as tape:
+    tape.watch(x)
+    z = tf.add(tf.multiply(w, x), b)
+    loss = tf.reduce_sum(tf.square(y - z))
+
+dloss_dx = tape.gradient(loss, x)
+tf.print("dL/dx:", dloss_dx)
