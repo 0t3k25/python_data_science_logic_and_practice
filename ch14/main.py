cd3e36a276c7df641bb696a39d2394745b0c2d98
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-02-20 23:22:01 +0900

    keep gradient

--- ch14/main.py
+++ ch14/main.py
@@ -108,3 +108,56 @@ with tf.GradientTape() as tape:
 
 dloss_dx = tape.gradient(loss, x)
 tf.print("dL/dx:", dloss_dx)
+
+with tf.GradientTape(persistent=True) as tape:
+    z = tf.add(tf.multiply(w, x), b)
+    loss = tf.reduce_sum(tf.square(y - z))
+
+dloss_dw = tape.gradient(loss, w)
+tf.print("dL/dw:", dloss_dw)
+dloss_db = tape.gradient(loss, b)
+tf.print("dL/db:", dloss_db)
+
+optimizer = tf.keras.optimizers.SGD()
+optimizer.apply_gradients(zip([dloss_dw, dloss_db], [w, b]))
+tf.print("Updated w:", w)
+tf.print("Updated bias:", b)
+
+# keras api
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(units=16, activation="relu"))
+model.add(tf.keras.layers.Dense(units=32, activation="relu"))
+## 遅延変数作成
+model.build(input_shape=(None, 4))
+model.summary()
+
+## モデルの変数を出力
+for v in model.variables:
+    print("{:20s}".format(v.name), v.trainable, v.shape)
+
+model = tf.keras.Sequential()
+model.add(
+    tf.keras.layers.Dense(
+        units=16,
+        activation=tf.keras.activations.relu,
+        kernel_initializer=tf.keras.initializers.glorot_uniform(),
+        bias_initializer=tf.keras.initializers.Constant(2.0),
+    )
+)
+model.add(
+    tf.keras.layers.Dense(
+        units=32,
+        activation=tf.keras.activations.sigmoid,
+        kernel_regularizer=tf.keras.regularizers.l1,
+    )
+)
+
+model.compile(
+    optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=[
+        tf.keras.metrics.Accuracy(),
+        tf.keras.metrics.Precision(),
+        tf.keras.metrics.Recall(),
+    ],
+)
