fee4e93ee78de2d22a1cf7cceee09300972b6c83
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-02-24 23:52:33 +0900

    create custom dim using keras api

--- ch13/main.py
+++ ch13/main.py
@@ -591,5 +591,44 @@ hist = model.fit(
     verbose=0,
 )
 
+
 # kerasを使った新しい層の定義
-class NoisyLinear()
+# kerasを使った新しい層の定義
+class NoisyLinear(tf.keras.layers.Layer):
+    def __init__(self, output_dim, noise_stddev=0.1, **kwargs):
+        self.output_dim = output_dim
+        self.noise_stddev = noise_stddev
+        super(NoiseyLinear, self).__init__(**kwargs)
+
+        def build(self, input_shape):
+            self.w = self.add_weight(
+                name="weights",
+                shape=(input_shape[1], self.output_dim),
+                initializer="random_normal",
+                trainable=True,
+            )
+            self.b = self.add_weight(
+                shape=(self.output_dim,), initializer="zeros", trainable=True
+            )
+
+        def call(self, inputs, training=False):
+            if training:
+                batch = tf.shape(input)[0]
+                dim = tf.shape(input)[1]
+                noise = tf.random.normal(
+                    shape=(batch, dim), mean=0.0, stddev=self.noise_stddev
+                )
+                noisy_inputs = tf.add(inputs, noise)
+
+            else:
+                noisy_inputs = inputs
+            z = tf.matmul(noisy_inputs, self.w) + self.b
+            return tf.keras.activations.relu(z)
+
+        def get_config(self):
+            config = super(NoisyLinear, self).get_config()
+            config.update(
+                {"output_dim": self.output_dim, "noise_stddev": self.noise_stddev}
+            )
+
+            return config
