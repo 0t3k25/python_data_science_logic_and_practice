1d0716f9c64f4fc276f30bc99889dcaa5d0c75d8
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-02-26 00:13:31 +0900

    solve xor using custom dim

--- ch13/main.py
+++ ch13/main.py
@@ -607,19 +607,20 @@ class NoisyLinear(tf.keras.layers.Layer):
             initializer="random_normal",
             trainable=True,
         )
+
         self.b = self.add_weight(
             shape=(self.output_dim,), initializer="zeros", trainable=True
         )
 
     def call(self, inputs, training=False):
         if training:
-            batch = tf.shape(input)[0]
-            dim = tf.shape(input)[1]
+            batch = tf.shape(inputs)[0]
+            dim = tf.shape(inputs)[1]
             noise = tf.random.normal(
                 shape=(batch, dim), mean=0.0, stddev=self.noise_stddev
             )
-            noisy_inputs = tf.add(inputs, noise)
 
+            noisy_inputs = tf.add(inputs, noise)
         else:
             noisy_inputs = inputs
         z = tf.matmul(noisy_inputs, self.w) + self.b
@@ -630,5 +631,53 @@ class NoisyLinear(tf.keras.layers.Layer):
         config.update(
             {"output_dim": self.output_dim, "noise_stddev": self.noise_stddev}
         )
-
         return config
+
+
+from mlxtend.plotting import plot_decision_regions
+
+tf.random.set_seed(1)
+model = tf.keras.Sequential(
+    [
+        NoisyLinear(4, noise_stddev=0.1),
+        tf.keras.layers.Dense(units=4, activation="relu"),
+        tf.keras.layers.Dense(units=4, activation="relu"),
+        tf.keras.layers.Dense(units=1, activation="sigmoid"),
+    ]
+)
+model.build(input_shape=(None, 2))
+model.summary()
+# コンパイル
+model.compile(
+    optimizer=tf.keras.optimizers.SGD(),
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=[tf.keras.metrics.BinaryAccuracy()],
+)
+# 訓練
+hist = model.fit(
+    x_train,
+    y_train,
+    validation_data=(x_valid, y_valid),
+    epochs=200,
+    batch_size=2,
+    verbose=0,
+)
+history = hist.history
+fig = plt.figure(figsize=(16, 4))
+ax = fig.add_subplot(1, 3, 1)
+plt.plot(history["loss"], lw=4)
+plt.plot(history["val_loss"], lw=4)
+plt.legend(["Train loss", "Validation loss"], fontsize=15)
+ax.set_xlabel("Epochs", size=15)
+ax = fig.add_subplot(1, 3, 2)
+plt.plot(history["binary_accuracy"], lw=4)
+plt.plot(history["val_binary_accuracy"], lw=4)
+plt.legend(["Train acc", "Validation Acc"], fontsize=15)
+ax.set_xlabel("Epoch", size=15)
+ax = fig.add_subplot(1, 3, 3)
+plot_decision_regions(X=x_valid, y=y_valid.astype(np.integer), clf=model)
+ax.set_xlabel(r"$x_1$", size=15)
+ax.xaxis.set_label_coords(1, -0.025)
+ax.set_ylabel(r"$x_2$", size=15)
+ax.yaxis.set_label_coords(-0.025, 1)
+plt.show()
