63274c5e79f22e4de68c884cea961fd3d8edffb3
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-03-09 23:59:16 +0900

    construc cnn using keras api

--- ch15/main.py
+++ ch15/main.py
@@ -126,10 +126,108 @@ mnist_train = mnist_train.skip(10000).batch(BATCH_SIZE)
 
 # kerasでのCNNの構築
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Conv2D(filters=32,kernel_size=(5,5), strides=(1,1),
-                                 padding='same',data_format='channels_last',
-                                 name='conv_1', activation='relu'))
-model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2),name='pool_1'))
-model.add(tf.keras.layers.Conv2D(filter=64,kernel_size=(5,5), strides=(1,1),
-                                 padding='same', name='conv_2d',activation='relu'))
-model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2),,name='pool_2'))
+model.add(
+    tf.keras.layers.Conv2D(
+        filters=32,
+        kernel_size=(5, 5),
+        strides=(1, 1),
+        padding="same",
+        data_format="channels_last",
+        name="conv_1",
+        activation="relu",
+    )
+)
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2), name="pool_1"))
+model.add(
+    tf.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=(5, 5),
+        strides=(1, 1),
+        padding="same",
+        name="conv_2d",
+        activation="relu",
+    )
+)
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2), name="pool_2"))
+
+# 特徴量mapのサイズを計算
+model.compute_output_shape(input_shape=(16, 28, 28, 1))
+
+# 層の平坦化
+model.add(tf.keras.layers.Flatten())
+model.compute_output_shape(input_shape=(16, 28, 28, 1))
+
+
+model.add(tf.keras.layers.Dense(units=1024, name="fc_1", activation="relu"))
+model.add(tf.keras.layers.Dropout(rate=0.5))
+model.add(tf.keras.layers.Dense(units=10, name="fc_2", activation="softmax"))
+
+tf.random.set_seed(1)
+model.build(input_shape=(None, 28, 28, 1))
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=["accuracy"],
+)
+
+history = model.fit(
+    mnist_train, epochs=NUM_EPOCHS, validation_data=mnist_valid, shuffle=True
+)
+
+# import numpy as np
+# 学習曲線を可視化
+import matplotlib.pyplot as plt
+
+hist = history.history
+x_arr = np.arange(len(hist["loss"])) + 1
+fig = plt.figure(figsize=(12, 4))
+ax = fig.add_subplot(1, 2, 1)
+ax.plot(x_arr, hist["loss"], "-o", label="Train loss")
+ax.plot(x_arr, hist["val_loss"], "--<", label="Validation loss")
+ax.set_xlabel("Epoch", size=15)
+ax.set_ylabel("Loss", size=15)
+ax.legend(fontsize=15)
+ax = fig.add_subplot(1, 2, 2)
+ax.plot(x_arr, hist["accuracy"], "-o", label="Train acc")
+ax.plot(x_arr, hist["val_accuracy"], "--<", label="Validation acc")
+ax.set_xlabel("Epoch", size=15)
+ax.set_ylabel("Accuracy", size=15)
+ax.legend(fontsize=15)
+plt.show()
+
+# 評価
+test_results = model.evaluate(mnist_test.batch(20))
+print("Test Acc.: {:.2f}%".format(test_results[1] * 100))
+
+# 入力値と予測ラベルの可視化
+batch_test = next(iter(mnist_test.batch(12)))
+preds = model(batch_test[0])
+tf.print(preds.shape)
+
+preds = tf.argmax(preds, axis=1)
+print(preds)
+
+fig = plt.figure(figsize=(12, 4))
+for i in range(12):
+    ax = fig.add_subplot(2, 6, i + 1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    img = batch_test[0][i, :, :, 0]
+    ax.imshow(img, cmap="gray_r")
+    ax.text(
+        0.9,
+        0.1,
+        "{}".format(preds[i]),
+        size=15,
+        color="blue",
+        horizontalalignment="center",
+        verticalalignment="center",
+        transform=ax.transAxes,
+    )
+plt.show()
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+celeba_bldr = tfds.builder("celeb_a")
+celeba_bldr.download_and_prepare()
