d004fb0e92e7e474f4cf38c3c954ad56e660d195
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2024-03-27 23:54:59 +0900

    implement  generator network and discriminator network

--- ch17/main.py
+++ ch17/main.py
@@ -11,3 +11,32 @@ else:
     device_name = "/CPU:0"
 
 print(device_name)
+
+import tensorflow as tfds
+import nunpy as np
+
+
+# 生成器ネットワークの関数を定義
+def make_generator_network(
+    num_hidden_layers=1, num_hidden_units=100, num_output_units=784
+):
+    model = tf.keras.Sequential()
+    for i in range(num_hidden_layers):
+        model.add(tf.keras.layers.Dense(units=num_hidden_units, use_bias=False))
+        model.add(tf.keras.layers.LeakyReLU())
+        model.add(tf.keras.layers.Dense(units=num_output_units, activation="tanh"))
+    return model
+
+
+# 識別器ネットワークの関数を定義
+def make_discriminator_network(
+    num_hidden_layers=1, num_hidden_units=100, num_output_units=1
+):
+    model = tf.keras.Sequential()
+    for i in range(num_hidden_layers):
+        model.add(tf.keras.layers.Dense(units=num_hidden_units))
+        model.add(tf.keras.layers.LeakyReLU())
+        model.add(tf.keras.layers.Dropout(rate=0.5))
+
+    model.add(tf.keras.layers.Dense(units=num_output_units, activation=None))
+    return model
